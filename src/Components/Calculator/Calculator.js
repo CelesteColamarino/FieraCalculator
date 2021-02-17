94cbd26130f374a05aa4904df0090f5515952d1b
Celeste Colamarino <celeste@Celestes-MacBook-Pro.local>
2021-02-17 17:57:01 -0300

    Added a method called addToOperator to keep track of the operators used and display them on the calculators screen. Created various states (operator, currentValue, previousValue

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -6,16 +6,33 @@ import "./calculator.css";
 
 const Calculator = () => {
   const [input, setInput] = useState(0);
+  const [operator, setOperator] = useState(null);
+  const [currentValue, setCurrentValue] = useState(null);
+  const [previousValue, setPreviousValue] = useState(null);
 
   const addToInput = (val) => {
-    const result = input ? input + val : val;
-    setInput(result);
+    const display =
+      input && input !== operator && input !== previousValue
+        ? input + val
+        : val;
+    setInput(display);
   };
 
   const clear = () => {
     setInput(0);
   };
 
+  const addToOperator = (val) => {
+    const auxOperators = ["+", "*", "/", "-"];
+    if (operator && auxOperators.indexOf(val)) {
+      setOperator(val);
+      setInput(val);
+    } else {
+      setOperator(val);
+      setPreviousValue(input);
+      setInput(val);
+    }
+  };
   return (
     <div className="wrapper">
       <div className="row">
@@ -25,25 +42,25 @@ const Calculator = () => {
         <Button handleClick={addToInput}>7</Button>
         <Button handleClick={addToInput}>8</Button>
         <Button handleClick={addToInput}>9</Button>
-        <Button handleClick={addToInput}>/</Button>
+        <Button handleClick={addToOperator}>/</Button>
       </div>
       <div className="row">
         <Button handleClick={addToInput}>4</Button>
         <Button handleClick={addToInput}>5</Button>
         <Button handleClick={addToInput}>6</Button>
-        <Button handleClick={addToInput}>*</Button>
+        <Button handleClick={addToOperator}>*</Button>
       </div>
       <div className="row">
         <Button handleClick={addToInput}>1</Button>
         <Button handleClick={addToInput}>2</Button>
         <Button handleClick={addToInput}>3</Button>
-        <Button handleClick={addToInput}>+</Button>
+        <Button handleClick={addToOperator}>+</Button>
       </div>
       <div className="row">
         <Button handleClick={addToInput}>.</Button>
         <Button handleClick={addToInput}>0</Button>
         <Button handleClick={addToInput}>=</Button>
-        <Button handleClick={addToInput}>-</Button>
+        <Button handleClick={addToOperator}>-</Button>
       </div>
       <div className="row">
         <ClearButton handleClick={clear}>Clear</ClearButton>
