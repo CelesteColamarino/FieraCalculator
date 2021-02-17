66fa722d6e5357b64361521f722838684d2d9fd3
Celeste Colamarino <celeste@Celestes-MacBook-Pro.local>
2021-02-17 14:06:24 -0300

    Input component added, with its css file. Imported useState to initialize an input state, and used setInput inside the new calculators method addToInput. Ternary added to the css button class to style operators with their own color

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./button.css";
 
-const Button = ({ children }) => {
-  return <div className="button">{children}</div>;
+const Button = ({ children, handleClick }) => {
+  const isOperator = (val) => {
+    return !isNaN(val) || val === "." || val === "=" ? "" : "operator";
+  };
+  return (
+    <div
+      className={`button ${isOperator(children)}`}
+      onClick={() => handleClick(children)}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Button;
